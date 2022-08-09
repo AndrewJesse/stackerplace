34fd99c1ffbd69f13eabde4e57dcb93398435dea
A.J. Marchese <aj.marchese@outlook.com>
2022-08-09 09:04:06 -0700

    greyed out search field text and changed it to say Search posts

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
+    <div class="flex flex-col space-y-3 filters md:flex-row md:space-y-0 md:space-x-6">
         <div class="w-full md:w-1/3">
-            <select wire:model="category" name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+            <select wire:model="category" name="category" id="category" class="w-full px-4 py-2 border-none rounded-xl">
                 <option value="All Categories">All Categories</option>
                 @foreach ($categories as $category)
                     <option value="{{ $category->name }}">{{ $category->name }}</option>
@@ -9,7 +9,7 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full px-4 py-2 border-none rounded-xl">
                 <option value="No Filter">No Filter</option>
                 <option value="Top Voted">Top Voted</option>
                 <option value="My Ideas">My Ideas</option>
@@ -19,9 +19,9 @@
                 @endadmin
             </select>
         </div>
-        <div class="w-full md:w-2/3 relative">
-            <input wire:model="search" type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
-            <div class="absolute top-0 flex itmes-center h-full ml-2">
+        <div class="relative w-full md:w-2/3">
+            <input wire:model="search" type="search" placeholder="Search posts" class="w-full px-4 py-2 pl-8 placeholder-gray-500 bg-white border-none rounded-xl">
+            <div class="absolute top-0 flex h-full ml-2 itmes-center">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
@@ -29,7 +29,7 @@
         </div>
     </div> <!-- end filters -->
 
-    <div class="ideas-container space-y-6 my-8">
+    <div class="my-8 space-y-6 ideas-container">
         @forelse ($ideas as $idea)
             <livewire:idea-index
                 :key="$idea->id"
@@ -37,9 +37,9 @@
                 :votesCount="$idea->votes_count"
             />
         @empty
-            <div class="mx-auto w-70 mt-12">
+            <div class="mx-auto mt-12 w-70">
                 <img src="{{ asset('img/no-ideas.svg') }}" alt="No Ideas" class="mx-auto mix-blend-luminosity">
-                <div class="text-gray-400 text-center font-bold mt-6">No ideas were found...</div>
+                <div class="mt-6 font-bold text-center text-gray-400">No ideas were found...</div>
             </div>
         @endforelse
     </div> <!-- end ideas-container -->
