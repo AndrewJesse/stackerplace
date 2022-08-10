349823e8c2278669562540f47abd6a7d677cd01c
A.J. Marchese <aj.marchese@outlook.com>
2022-08-09 20:37:12 -0700

    hid the ideas tabs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
             </div>
 
             <div class="w-full px-2 md:px-0 md:w-175">
-                <livewire:status-filters />
+                {{-- <livewire:status-filters /> --}}
 
                 <div class="mt-8">
                     {{ $slot }}
