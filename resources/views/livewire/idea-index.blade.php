669aa10d4c0e5f31b6e0fdf0e91ee54a58068810
A.J. Marchese <aj.marchese@outlook.com>
2022-08-11 08:55:29 -0700

    changed the vote button to say like

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -10,43 +10,43 @@
             clicked.closest('.idea-container').querySelector('.idea-link').click()
         }
     "
-    class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer"
+    class="flex transition duration-150 ease-in bg-white cursor-pointer idea-container hover:shadow-card rounded-xl"
 >
-    <div class="hidden md:block border-r border-gray-100 px-5 py-8">
+    <div class="hidden px-5 py-8 border-r border-gray-100 md:block">
         <div class="text-center">
             <div class="font-semibold text-2xl @if ($hasVoted) text-blue @endif">{{ $votesCount }}</div>
-            <div class="text-gray-500">Votes</div>
+            <div class="text-gray-500">Likes</div>
         </div>
 
         <div class="mt-8">
             @if ($hasVoted)
-                <button wire:click.prevent="vote" class="w-20 bg-blue text-white border border-blue hover:bg-blue-hover font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Voted</button>
+                <button wire:click.prevent="vote" class="w-20 px-4 py-3 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue hover:bg-blue-hover text-xxs rounded-xl">Unlike</button>
             @else
-                <button wire:click.prevent="vote" class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+                <button wire:click.prevent="vote" class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 hover:border-gray-400 text-xxs rounded-xl">Like</button>
             @endif
         </div>
     </div>
-    <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
+    <div class="flex flex-col flex-1 px-2 py-6 md:flex-row">
         <div class="flex-none mx-2 md:mx-0">
             <a href="#">
                 <img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
             </a>
         </div>
-        <div class="w-full flex flex-col justify-between mx-2 md:mx-4">
-            <h4 class="text-xl font-semibold mt-2 md:mt-0">
+        <div class="flex flex-col justify-between w-full mx-2 md:mx-4">
+            <h4 class="mt-2 text-xl font-semibold md:mt-0">
                 <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">{{ $idea->title }}</a>
             </h4>
-            <div class="text-gray-600 mt-3 line-clamp-3">
+            <div class="mt-3 text-gray-600 line-clamp-3">
                 @admin
                     @if ($idea->spam_reports > 0)
-                        <div class="text-red mb-2">Spam Reports: {{ $idea->spam_reports }}</div>
+                        <div class="mb-2 text-red">Spam Reports: {{ $idea->spam_reports }}</div>
                     @endif
                 @endadmin
                 {{ $idea->description }}
             </div>
 
-            <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
-                <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+            <div class="flex flex-col justify-between mt-6 md:flex-row md:items-center">
+                <div class="flex items-center space-x-2 text-xs font-semibold text-gray-400">
                     <div>{{ $idea->created_at->diffForHumans() }}</div>
                     <div>&bull;</div>
                     <div>{{ $idea->category->name }}</div>
@@ -55,27 +55,27 @@
                 </div>
                 <div
                     x-data="{ isOpen: false }"
-                    class="flex items-center space-x-2 mt-4 md:mt-0"
+                    class="flex items-center mt-4 space-x-2 md:mt-0"
                 >
                     <div class="{{ 'status-'.Str::kebab($idea->status->name) }} text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">{{ $idea->status->name }}</div>
                 </div>
 
-                <div class="flex items-center md:hidden mt-4 md:mt-0">
-                    <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                <div class="flex items-center mt-4 md:hidden md:mt-0">
+                    <div class="h-10 px-4 py-2 pr-8 text-center bg-gray-100 rounded-xl">
                         <div class="text-sm font-bold leading-none @if ($hasVoted) text-blue @endif">{{ $votesCount }}</div>
-                        <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                        <div class="font-semibold leading-none text-gray-400 text-xxs">Votes</div>
                     </div>
                     @if ($hasVoted)
                         <button
                             wire:click.prevent="vote"
-                            class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5"
+                            class="w-20 px-4 py-3 -mx-5 font-bold text-white uppercase transition duration-150 ease-in border bg-blue border-blue text-xxs rounded-xl hover:bg-blue-hover"
                         >
                             Voted
                         </button>
                     @else
                         <button
                             wire:click.prevent="vote"
-                            class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
+                            class="w-20 px-4 py-3 -mx-5 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs rounded-xl hover:border-gray-400"
                         >
                             Vote
                         </button>
